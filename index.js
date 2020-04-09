e1a1b666793925729826345d4da773ff44a386e0
Awhit-commits <ndrw_whtmr@ymail.com>
2020-04-09 14:43:01 -0500

    added comments in api file

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+//importing express files for the server
 const express = require('express');
 
+//storing port number in a variable
 const port = 8000;
 
+//Utilzing express module as needed
 const app = express();
 
+
+//Importing the api file
 let api = require('./routes/api')
 
+//Importing the Admin file
 let admin = require('./routes/admin')
 
 
-
+//Setting the api route
 app.use('/api',api);
 
+//Setting the admin route
 app.use('/admin',admin);
+
+
 app.listen(port,console.log(`Listening on port ${port}`))
 
--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,24 @@
 const express = require('express');
-
+//Importing the router from the express module
 const router = express.Router();
 
-
+//Utitlzing the json method from express
 router.use(express.json())
+
+//Defining the get request at a specific ID
 
 router.get('/:id',(req,res)=>{
     res.send(`You attemped to view item ${req.params.id}`);
 
 })
 
+//Defining the get request that shows all items
+
 router.get('/',(req,res)=>{
     res.send(`You attemped to view all items`);
 
 })
-
+//Defining Post that creates a json object
 router.post('/:id',(req,res)=>{
     let jsonApi = {id:req.params.id,
     description:"Mock description",
